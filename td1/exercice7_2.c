326a2a9f68aab2d483172f31808c96803eb49e23
Jeremy Perthuis <jeremy_perthuis@outlook.com>
2017-02-22 10:54:27 +0100

    modif exo 7

--- td1/exercice7_2.c
+++ td1/exercice7_2.c
@@ -31,8 +31,24 @@ FICHIER my_open(char* pathname,int mode)
 
 int main(int argc, char *argv[])
 {
+	FICHIER *f1, *f2;
+	int c;
 
+	f1=my_open(argv[1],"r");
+	if(f1==NULL){
 
+		perror("Erreur ouverture 1er Fichier");
+		exit(1);
+	}
+
+	f1=my_open(argv[2],"w");
+	if(f1==NULL){
+
+		perror("Erreur ouverture 2eme Fichier");
+		exit(1);
+	}
+
+	while((c=mygetc))
 			
    exit(EXIT_SUCCESS);
            
