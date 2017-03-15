65a38a22d7169c0868ff13f98f0d2c7b6d160084
Jeremy Perthuis <jeremy_perthuis@outlook.com>
2017-03-15 12:23:03 +0100

    modif exo 4

--- td2/exercice4.c
+++ td2/exercice4.c
@@ -4,19 +4,34 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/times.h>
+#include <time.h>
+
+// Executer le programme avec un chemin de dossier
 
 
-
-int main(int argc, char** argv)
+void discount_ls(char *arg)
 {
-	char * commande = "ls ";
+	char * commande = "ls -R ";
 	char *res = NULL;
-	res=malloc(sizeof(char)*(strlen(argv[1])+strlen(commande)));
+	res=malloc(sizeof(char)*(strlen(arg)+strlen(commande)));
 	// on concatene l'argument avec la chaine "ls"
 	strcat(res,commande);
-	strcat(res,argv[1]);
+	strcat(res,arg);
 	printf("res=%s\n",res);
 	// on invoque la commande
 	system(res);	
+}
+
+int main(int argc, char** argv)
+{
+	struct tms start;
+	struct tms stop;
+
+	times(&start);
+	discount_ls(argv[1]);
+	times(&stop);
+	clock_t t0 = (stop.tms_cutime) - (start.tms_cutime);
+	printf("%d\n", (int)(t0));
 }
 
